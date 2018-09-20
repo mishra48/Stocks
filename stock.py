8244ca07071da7ca873c94f09c767c0f211adb21
Kartikeya Mishra <mishra48@purdue.edu>
2018-09-20 05:42:42 -0400

    added timer

--- stock.py
+++ stock.py
@@ -1,13 +1,28 @@
 from iex import Stock
 import telegram_send
+import sched,time
 
 
+def print_stocks():
+    fit = int(Stock("FIT").price())
+    tes = int(Stock("TSLA").price())
+    if fit > 5:
+        telegram_send.send(messages=['FIT: '+str(fit)])
+
+    if  tes > 200:
+        telegram_send.send(messages=['TSLA: ' + str(tes)])
 
 
-if int(Stock("FIT").price())> 0:
-    telegram_send.send(messages=['FIT: '+str(Stock("FIT").price())])
+if __name__ == '__main__':
 
-if int(Stock("TSLA").price()) > 0:
-    telegram_send.send(messages=['TSLA: ' + str(Stock("TSLA").price())])
+
+    s = sched.scheduler(time.time, time.sleep)
+    while 1:
+        s.enter(10,1,print_stocks)
+        s.run()
+
+        print_stocks()
+
+
 
 
